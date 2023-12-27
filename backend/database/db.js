d391cc12a20d060605b6c45f7edbd68c906d3d6a
johnmuthua <caveomuthama@gmail.com>
2023-12-27 12:47:37 +0300

    chore: remove deprecation warnings

--- backend/database/db.js
+++ backend/database/db.js
@@ -6,6 +6,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true,
     });
     console.log(
       `Mongodb connected running on ${conn.connection.host}`.cyan.underline
