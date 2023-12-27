0d0092352219eae19014d58ae4b3586ae57d7419
johnmuthua <caveomuthama@gmail.com>
2023-12-27 09:36:51 +0300

    Add chats and home components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Chats from "./pages/Chats";
 
 function App() {
   return (
     <div className="App">
       <Route path="/" component={Home} exact />
+      <Route path="/chats" component={Chats} exact />
     </div>
   );
 }
