527eb091fb58b327d2ce024b92ffdb65cd9aafe3
johnmuthua <caveomuthama@gmail.com>
2023-12-27 12:23:50 +0300

    Add routers

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,16 @@ const express = require("express");
 const connectDB = require("./database/db");
 const dotenv = require("dotenv");
 const colors = require("colors");
-
+const userRouter = require("./routes/userRoute");
 dotenv.config();
 
 connectDB();
 const app = express();
 
-app.get("/api/", (req, res) => {
-  res.send("The api is running");
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  console.log(req.params);
-});
+app.use("/api/user/", userRouter);
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(
   PORT,
   console.log(` The server is running on ${PORT} `.bgMagenta.bold)
