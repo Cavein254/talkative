18fda1c3a5b8d20aaada75408d883dc6f3b043e1
johnmuthua <caveomuthama@gmail.com>
2023-12-27 10:07:06 +0300

    add map function for chats

--- frontend/src/pages/ChatsPage.js
+++ frontend/src/pages/ChatsPage.js
@@ -1,7 +1,32 @@
 import React from "react";
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const ChatsPage = () => {
-  return <div>ChatsPage</div>;
+  const [chats, setchats] = useState([]);
+  const fetchChats = async () => {
+    const url = "https://fakestoreapi.com/products/category/jewelery";
+    const { data } = await axios.get(url);
+    setchats(data);
+  };
+
+  useEffect(() => {
+    fetchChats();
+  }, []);
+  return (
+    <div>
+      {chats
+        ? chats?.map((item) => {
+            return (
+              <div key={item.id}>
+                <h1>{item.title}</h1>
+              </div>
+            );
+          })
+        : "Still loading assets"}
+    </div>
+  );
 };
 
 export default ChatsPage;
