459071fdd887b5b43a69ce05a14a9645d979fbf2
johnmuthua <caveomuthama@gmail.com>
2023-12-28 11:47:37 +0300

    Add rename chat

--- backend/routes/chatRoute.js
+++ backend/routes/chatRoute.js
@@ -1,11 +1,15 @@
 const router = require("express").Router();
 const isUserAuthenticated = require("../middleware/authmiddleware");
-const { accessChats, fetchChats } = require("../controllers/chatController");
+const {
+  accessChats,
+  fetchChats,
+  createGroupChat,
+} = require("../controllers/chatController");
 
 router.route("/").get(isUserAuthenticated, fetchChats);
 router.route("/").post(isUserAuthenticated, accessChats);
-// router.route("/group").get(isUserAuthenticated, createGroupChat);
-// router.route("/rename").put(isUserAuthenticated, renameGroupChat);
+router.route("/group").get(isUserAuthenticated, createGroupChat);
+router.route("/rename").put(isUserAuthenticated, renameGroupChat);
 // router.route("/leave").put(isUserAuthenticated, leaveGroupChat);
 // router.route("/add").put(isUserAuthenticated, addToGroupChat);
 
